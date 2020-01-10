4f4426231183fc7eb97791797c160fe835866df0
hongka <karl.hong@cargesmart.com>
2020-01-10 16:55:57 +0800

    nothing

---
+++ src/main/java/mkii/finger1/RemoveNodeOfLinked.java
@@ -0,0 +1,25 @@
+package mkii.finger1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 删除链表的节点
+ * 给定单向链表的表头指针和一个节点的指针，定义一个函数在O(1)时间内删除该节点。
+ *
+ * 测试：
+ * 1.删除了头结点
+ * 2.删除了尾节点
+ * 3.删除了中间节点
+ */
+public class RemoveNodeOfLinked {
+    public void removeNode(ListNode head, ListNode node){
+        // current->pre
+        Map<ListNode, ListNode> map = new HashMap<>();
+
+
+
+
+
+    }
+}
