b250178c5d4a7e6e7e6d497ae7e82666f6cee373
hongka <karl.hong@cargesmart.com>
2020-01-22 09:07:22 +0800

    map端实现join complete

--- src/main/java/mkii/hadoop/mapreduce/join/in/map/JoinMapper.java
+++ src/main/java/mkii/hadoop/mapreduce/join/in/map/JoinMapper.java
@@ -1,5 +1,6 @@
 package mkii.hadoop.mapreduce.join.in.map;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
@@ -9,18 +10,17 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.util.StringUtils;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class JoinMapper extends Mapper<LongWritable, Text, Text, Text> {
 
     private Map<String, String> map;
 
-    // 在初始化map时调用，先加载小表到内存中
+    // 在初始化mapper时调用，先加载小表到内存中
     @Override
     protected void setup(Context context) throws IOException, InterruptedException {
         map = new HashMap<>();
@@ -30,22 +30,28 @@ public class JoinMapper extends Mapper<LongWritable, Text, Text, Text> {
             // 会打印出全文件名
             System.out.println(cacheFile);
 
+            // 通过hadoop的FSDataInputStream读取hdfs数据，并放入内存hashMap中
             Configuration configuration = context.getConfiguration();
             FileSystem fileSystem = FileSystem.get(configuration);
             FSDataInputStream fsDataInputStream = fileSystem.open(new Path(cacheFile.getPath()));
-
-
+            List<String> lines = IOUtils.readLines(fsDataInputStream);
+            for (String line : lines) {
+                String[] fields = StringUtils.split(line, '\t');
+                if ("id".equals(fields[0])){
+                    continue;
+                }
+                map.put(fields[0], line);
+            }
         }
 
         // 从本地读取文件，加载小表到内存，hashMap
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("D:\\Code\\daily-study\\doc\\hadoop\\mapreduce\\joinCase\\t_product"));
-
+        /*BufferedReader bufferedReader = new BufferedReader(new FileReader("D:\\Code\\daily-study\\doc\\hadoop\\mapreduce\\joinCase\\t_product"));
 
         String s;
         while ((s = bufferedReader.readLine()) != null){
             String[] split = s.split("\t");
             map.put(split[0], split[1] + "\t" + split[2] + "\t" + split[3]);
-        }
+        }*/
     }
 
     @Override
--- src/main/java/mkii/hadoop/mapreduce/join/in/map/JoinRunner.java
+++ src/main/java/mkii/hadoop/mapreduce/join/in/map/JoinRunner.java
@@ -14,7 +14,8 @@ import java.io.IOException;
  * select a.id,a.date,b.name,b.category_id,b.price from t_order a join t_product b on a.pid = b.id
  *
  * 注意：
- * bean类的序列化与反序列化问题
+ * 1.bean类的序列化与反序列化问题
+ * 2.从缓存中得到hdfs中的文件名，再利用流读取hdfs中的文件
  *
  */
 public class JoinRunner {
@@ -26,14 +27,13 @@ public class JoinRunner {
         job.setJarByClass(mkii.hadoop.mapreduce.join.in.map.JoinRunner.class);
 
         job.setMapperClass(JoinMapper.class);
-        //job.setReducerClass(JoinReducer.class);
 
-        // 设置reduce任务数为0
+        // 设置reduce任务数为0。因为在map端完成join操作
         job.setNumReduceTasks(0);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
-        // 先把小表的文件加入缓存中
+        // 先把小表的文件加入缓存中。实际上这里只是将小表的文件名放入缓存，真正读取文件内容是在初始化mapper时调用setup()方法
         job.addCacheFile(new Path("/join/reduce/input/t_product").toUri());
 
         FileInputFormat.setInputPaths(job, new Path("/join/reduce/input/t_order"));
