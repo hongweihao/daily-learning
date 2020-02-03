2e2d05dee8ddf43997f144d640c6ccc4b479ba64
hongweihao <1245215272@qq.com>
2020-02-03 09:35:45 +0800

    remove bom with commons-io lib

--- src/main/java/mkii/io/ChangeEncoding.java
+++ src/main/java/mkii/io/ChangeEncoding.java
@@ -1,5 +1,7 @@
 package mkii.io;
 
+import org.apache.commons.io.input.BOMInputStream;
+
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -8,15 +10,15 @@ import java.nio.charset.UnsupportedCharsetException;
 public class ChangeEncoding {
     public static void main(String[] args) throws Exception {
 
-        String inputString = LocalFileUtil.readBigFile("C:\\Users\\HONGKA\\Downloads\\Kaiser_Shipment_Milestone_Update_Report20200114.csv");
+        String inputString = readFileWithoutBom("D:\\work\\Kaiser_Shipment_Milestone_Update_Report_CSV20200117010005933_MB202001170108320001.csv");
 
         String res = new String(inputString.getBytes(), StandardCharsets.ISO_8859_1);
 
         //LocalFileUtil.writeToFileWithEncoding("C:\\Users\\HONGKA\\Downloads\\kaiser.csv", res, "ISO-8859-1");
 
         //File file = new File("C:\\Users\\HONGKA\\Downloads\\k.csv");
-        File file = new File("C:\\Users\\HONGKA\\Downloads\\l.csv");
-        if (!file.exists()){
+        File file = new File("D:\\work\\l.csv");
+        if (!file.exists()) {
             file.createNewFile();
         }
         FileOutputStream fileOutputStream = new FileOutputStream(file);
@@ -27,11 +29,10 @@ public class ChangeEncoding {
     }
 
     /**
-     *
-     * @param source full source file name
-     * @param dest full dest file name
+     * @param source      full source file name
+     * @param dest        full dest file name
      * @param fromCharSet source file encoding
-     * @param toCharSet dest file encoding
+     * @param toCharSet   dest file encoding
      * @throws Exception
      */
     public static void changeEncoding(String source, String dest, String fromCharSet, String toCharSet) throws Exception {
@@ -65,7 +66,7 @@ public class ChangeEncoding {
     public static void saveFileWithEncoding(String dest, String content, String encoding) throws IOException {
         File file = new File(dest);
 
-        if (!file.exists()){
+        if (!file.exists()) {
             file.createNewFile();
         }
         if (!Charset.isSupported(encoding)) {
@@ -82,6 +83,30 @@ public class ChangeEncoding {
             assert outWrite != null;
             outWrite.close();
         }
+    }
+
+    // read file content. depends common-io jar
+    public static String readFileWithoutBom(String source) throws IOException {
+        File file = new File(source);
+
+        InputStream inputStream = null;
+        InputStreamReader reader = null;
+        String str;
+        try {
+            inputStream = new FileInputStream(file);
+            // 使用commons-io去除bom头
+            BOMInputStream bomInputStream = new BOMInputStream(inputStream);
+            reader = new InputStreamReader(bomInputStream);
+
+            char[] chs = new char[(int) file.length()];
+            reader.read(chs);
+            str = new String(chs).trim();
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+        return str;
     }
 
 }
