3a2f5b7a1a18179b3615a822e7e7c7587262dac4
karl.hong <karl.hong@youdu.im>
2020-12-29 10:07:05 +0800

    feat(excel): to create a excel file

--- go_demo/src/excel/excel.go
+++ go_demo/src/excel/excel.go
@@ -16,11 +16,11 @@ func ExportExcel() *xlsx.File {
 		BottomColor: "5B9BD5",
 	}
 
-	tStyle := xlsx.NewStyle()
-	tStyle.Alignment.Horizontal = "Center"
-	tStyle.Alignment.Vertical = "Center"
-	tStyle.Border = border
-	tStyle.Font.Bold = true
+	titleStyle := xlsx.NewStyle()
+	titleStyle.Alignment.Horizontal = "Center"
+	titleStyle.Alignment.Vertical = "Center"
+	titleStyle.Border = border
+	titleStyle.Font.Bold = true
 
 	bgStyle := xlsx.NewStyle()
 	bgStyle.Border = border
@@ -28,44 +28,101 @@ func ExportExcel() *xlsx.File {
 
 	bdStyle := xlsx.NewStyle()
 	bdStyle.Border = border
+	bdStyle.Alignment.Horizontal = "left"
+	bdStyle.Alignment.Vertical = "top"
+	bdStyle.ApplyFill = true
 
-	excel := xlsx.NewFile()
-	sheet, _ := excel.AddSheet("vote")
+	footerStyle := xlsx.NewStyle()
+	footerStyle.Alignment.Horizontal = "Center"
+	footerStyle.Alignment.Vertical = "Center"
+	footerStyle.Border = border
+
+	exportXlsx := NewExportXlsx()
+	sheet := exportXlsx.AddSheet("sheet")
 
 	titleRow := sheet.AddRow()
 	titleCell := titleRow.AddCell()
 	titleCell.Merge(3, 0)
-	titleCell.SetString("投票标题")
-	titleCell.SetStyle(tStyle)
+	titleCell.SetString("大标题")
+	titleCell.SetStyle(titleStyle)
+	exportXlsx.AddEmptyCell(titleRow, titleStyle, 3)
 
-	headerRow := sheet.AddRow()
-	headers := []string{"选项", "票数", "投票人员", "百分比"}
-	for _, header := range headers {
-		cell := headerRow.AddCell()
-		cell.SetString(header)
-		cell.SetStyle(bgStyle)
+	exportXlsx.AddRow(sheet, bgStyle, "标题一", "标题二", "标题三", "占比")
+
+	exportXlsx.MergeRow(sheet, bdStyle, 1, "选项一", "xxx", "张三", "28.99%")
+	exportXlsx.OnlyUserRow(sheet, "李四", bdStyle)
+
+	exportXlsx.MergeRow(sheet, bdStyle, 2, "选项二", "xxx", "xxx", "28.99%")
+	exportXlsx.OnlyUserRow(sheet, "xxx", bdStyle)
+	exportXlsx.OnlyUserRow(sheet, "xxx", bdStyle)
+
+	exportXlsx.MergeRow(sheet, bdStyle, 1, "选项三", "xxx", "bbb", "28.99%")
+	exportXlsx.OnlyUserRow(sheet, "bbb", bdStyle)
+
+	row := sheet.AddRow()
+	cell := row.AddCell()
+	cell.SetString("表脚")
+	cell.SetStyle(footerStyle)
+	cell.Merge(3, 0)
+	exportXlsx.AddEmptyCell(row, footerStyle, 3)
+
+	return exportXlsx.Excel
+}
+
+type ExportXlsx struct {
+	Excel *xlsx.File
+}
+
+func NewExportXlsx() *ExportXlsx {
+	return &ExportXlsx{Excel: xlsx.NewFile()}
+}
+
+func (ex *ExportXlsx) AddSheet(name string) *xlsx.Sheet {
+	sheet, _ := ex.Excel.AddSheet(name)
+	return sheet
+}
+
+func (ex *ExportXlsx) AddRow(sheet *xlsx.Sheet, style *xlsx.Style, options ...string) *xlsx.Row {
+	row := sheet.AddRow()
+	for _, option := range options {
+		cell := row.AddCell()
+		cell.SetString(option)
+		cell.SetStyle(style)
 	}
+	return row
+}
 
-	row1 := sheet.AddRow()
-	cell1 := row1.AddCell()
-	cell1.Merge(0, 1)
-	cell1.SetString("投票选项")
-	cell1.SetStyle(bdStyle)
+func (ex *ExportXlsx) MergeRow(sheet *xlsx.Sheet, style *xlsx.Style, mergeLine int, options ...string) *xlsx.Row {
+	row := sheet.AddRow()
+	for index, option := range options {
+		cell := row.AddCell()
+		cell.SetStyle(style)
+		if index != 2 {
+			cell.Merge(0, mergeLine)
+		}
+		cell.SetString(option)
+	}
+	return row
+}
 
-	cell2 := row1.AddCell()
-	cell1.Merge(0, 1)
-	cell2.SetString("2")
-	cell2.SetStyle(bdStyle)
+func (ex *ExportXlsx) OnlyUserRow(sheet *xlsx.Sheet, user string, style *xlsx.Style) *xlsx.Row {
+	row := sheet.AddRow()
 
-	cell31 := row1.AddCell()
-	cell1.Merge(0, 1)
-	cell31.SetString("zhangsan")
-	cell31.SetStyle(bdStyle)
+	row.AddCell().SetStyle(style)
+	row.AddCell().SetStyle(style)
 
-	cell4 := row1.AddCell()
-	cell4.SetString("%")
-	cell4.SetStyle(bdStyle)
+	cell31 := row.AddCell()
+	cell31.SetString(user)
+	cell31.SetStyle(style)
 
-	return excel
+	row.AddCell().SetStyle(style)
+	return row
+}
 
+func (ex *ExportXlsx) AddEmptyCell(row *xlsx.Row, style *xlsx.Style, count int) *xlsx.Row {
+	for i := 0; i < count; i++ {
+		cell := row.AddCell()
+		cell.SetStyle(style)
+	}
+	return row
 }
--- go_demo/src/main.go
+++ go_demo/src/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"go_demo/src/excel"
+)
+
 func main() {
 	/*sendEmail := email.NewSendEmail("Karl", "****", "smtp.exmail.qq.com", "from@xxx.com", 465)
 	err := sendEmail.SendEmail("subject", "message", "to@xxx.com")
@@ -8,13 +13,14 @@ func main() {
 		return
 	}*/
 
-	/*file := excel.ExportExcel()
+	file := excel.ExportExcel()
 	err := file.Save("d:/test.xls")
 	if err != nil {
 		fmt.Println("failed")
 		return
 	}
+	fmt.Println("successful")
 
-	fmt.Println("successful")*/
-	arraySlice()
+	//arraySlice()
+
 }
