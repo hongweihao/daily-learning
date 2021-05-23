64cf68c5250c8a57f8b314b305b1c7afc702b1f0
hongweihao <1245215272@qq.com>
2021-05-23 09:08:57 +0800

    refactor: return string

--- go_demo/src/gee-web/day3-router/main.go
+++ go_demo/src/gee-web/day3-router/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gee-web/day3-router/gee"
+	"net/http"
 )
 
 func main() {
@@ -14,38 +15,33 @@ func main() {
 	engine.Run(":8080")
 }
 
-func allHandler(c *gee.Context) {
-	c.Rw.Write([]byte("All Get here"))
-}
-
 func indexHandler(c *gee.Context) {
-	c.Rw.Write([]byte("Welcome"))
+	c.String(http.StatusOK, "Welcome")
 }
 
 func helloHandler(c *gee.Context) {
-	c.Rw.Write([]byte("Hello world"))
+	c.String(http.StatusOK, "Hello world")
 }
 
 func postHandler(c *gee.Context) {
-	c.Rw.Write([]byte("This is a POST request"))
+	c.String(http.StatusOK, "This is a post request")
 }
 
 func paramHandler(c *gee.Context) {
 	lang, ok := c.ParamGet("lang")
 	if !ok {
-		c.Rw.Write([]byte("cannot read lang"))
+		c.String(http.StatusOK, "Cannot read lang")
 		return
 	}
-
-	c.Rw.Write([]byte("read lang(" + lang + ") from " + c.Path))
+	c.String(http.StatusOK, "Get lang("+lang+") from "+c.Path)
 }
 
 func filePathHandler(c *gee.Context) {
 	filepath, ok := c.ParamGet("filepath")
 	if !ok {
-		c.Rw.Write([]byte("cannot read filepath"))
+		c.String(http.StatusOK, "Cannot read filepath")
 		return
 	}
 
-	c.Rw.Write([]byte("read filepath(" + filepath + ") from " + c.Path))
+	c.String(http.StatusOK, "Read filepath("+filepath+") from "+c.Path)
 }
