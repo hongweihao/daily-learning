9f13b172b29d11451088f52a45b74dc042d78585
mkii <karl.hong@honso.co.jp>
2021-06-03 09:18:44 +0800

    test for integration template into day7-recovery

--- go_demo/src/gee-web/day7-recovery/main.go
+++ go_demo/src/gee-web/day7-recovery/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"gee-web/day7-recovery/gee"
+	"html/template"
 	"net/http"
+	"time"
 )
+
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
 
 func main() {
 	r := gee.Default()
@@ -11,6 +24,35 @@ func main() {
 		names := []string{"mkii"}
 		// panic: out of range
 		c.String(http.StatusOK, names[4])
+	})
+
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+
+	// 加载模板文件
+	r.LoadHTMLGlob("templates/*")
+	// 加载静态文件
+	r.Static("/assets", "./static")
+
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", nil)
+	})
+
+	stu1 := &student{Name: "Geektutu", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
+	r.GET("/students", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "arr.tmpl", gee.H{
+			"title":  "gee",
+			"stuArr": [2]*student{stu1, stu2},
+		})
+	})
+
+	r.GET("/date", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
+			"title": "gee",
+			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
+		})
 	})
 
 	r.Run(":8888")
